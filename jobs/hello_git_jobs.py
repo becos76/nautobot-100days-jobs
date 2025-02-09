01a2243e103f7d5abc7eb87b681f9fb4cb679280
Manolis <mkaliotis@gmail.com>
2025-02-09 21:08:14 +0200

    Update job name and descr

--- jobs/hello_git_jobs.py
+++ jobs/hello_git_jobs.py
@@ -1,9 +1,12 @@
 from nautobot.apps.jobs import Job
 
+name = "Git Jobs"
+
 class HelloGitJobs(Job):
 
     class Meta: 
         name = "Hello Jobs from Git Repo"
+        description = "Jobs synced from git"
 
     def run(self):
         self.logger.debug("This is from the Git repo.")
